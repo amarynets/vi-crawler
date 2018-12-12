25c196cb6b38d04706374367e59fba1c1f7ca80d
Andrii Marynets <andrii.marynets@gmail.com>
2018-12-13 01:15:09 +0200

    Add pipeline

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
 
 
 class CrawlerPipeline(object):
+
+    def open_spider(self, spider):
+        self.file_names = ['RegionItem', 'UniversityItem', 'SpecialtyItem']
+        self.files = {k: open(f'{k}.json', 'w', encoding='utf-8') for k in self.file_names}
+        for file in self.files.values():
+            file.write('[\n')
+
+        self.statuses = {k: True for k in self.file_names}
+
+    def close_spider(self, spider):
+        for file in self.files.values():
+            file.write('\n]')
+            file.close()
+
     def process_item(self, item, spider):
+        if self.statuses[item.__class__.__name__]:
+            self.statuses[item.__class__.__name__] = False
+            json.dump(dict(item), self.files[item.__class__.__name__], indent=4, ensure_ascii=False)
+        else:
+            self.files[item.__class__.__name__].write(',\n')
+            json.dump(dict(item), self.files[item.__class__.__name__], indent=4, ensure_ascii=False)
         return item
